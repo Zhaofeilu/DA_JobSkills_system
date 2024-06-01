76f8e9cc8f42bab305708d804e9b6f2fc82ebed0
Zhaofeilu <1328031967@qq.com>
2024-06-02 02:55:20 +0800

    Add files via upload
    Project Sprint 1

---
+++ linkedin_mysql.sql
@@ -0,0 +1,10 @@
+
+DROP DATABASE IF EXISTS linkedin;
+
+CREATE DATABASE IF NOT EXISTS linkedin;
+
+USE linkedin;
+
+DROP TABLE IF EXISTS address, job, skills, job_skill;
+    
+select * from job_skill;
